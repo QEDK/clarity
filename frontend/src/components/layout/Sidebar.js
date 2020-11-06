3626c37ad9ef6fba44da78a05243802d0e2127e0
Preet Shah <shahpreetk@gmail.com>
2020-11-06 18:35:04 +0530

    frontend: add list and journal entry

--- frontend/src/components/layout/Sidebar.js
+++ frontend/src/components/layout/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa'
+import { FaChevronDown, FaInbox, FaRegCalendar } from 'react-icons/fa'
 import { useSelectedProjectValue } from '../../context';
 import Projects from '../Projects';
 import AddProject from '../AddProjects';
@@ -28,14 +28,6 @@ const Sidebar = () => {
                     setSelectedProject('TODAY')
                 }} >
                     <span><FaRegCalendar /></span><span>Today</span></div></li>
-                <li data-testid='next_7' className={active === 'next_7' ? 'active' : undefined}>
-                    <div aria-label='Show Tasks for the Next 7 days' tabIndex={0} role='button' onClick={() => {
-                    setActive('next_7');
-                    setSelectedProject('NEXT_7')
-                }} onKeyDown={() => {
-                    setActive('next_7');
-                    setSelectedProject('NEXT_7')
-                }} ><span><FaRegCalendarAlt /></span><span>Next 7 Days</span></div></li>
             </ul>
 
             <div className="sidebar__middle"aria-label='Show/Hide Projects'  onClick={()=>setShowProjects(!showProjects)} onKeyDown={()=>setShowProjects(!showProjects)} tabIndex={0} role='button'>
