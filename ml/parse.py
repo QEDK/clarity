6194db310b688e53b0c0f84c3cf5d2570283bbd5
QEDK <1994constant@gmail.com>
2020-11-07 01:10:04 +0530

    ml: Add TF-IDF support

--- ml/parse.py
+++ ml/parse.py
@@ -1,13 +1,45 @@
+import asyncio
+import json
 import spacy
-from spacy import displacy
 import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from spacy import displacy
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 
-def get_formatted_entities(text):
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(text)
-    return displacy.render(doc, style="ent")
+class processText():
+    def __init__(self, text: str):
+        self.text = text
+        self.nlp = spacy.load("en_core_web_md")
+        self.doc = self.nlp(text)
+
+    async def process(self):
+        fmt_ents, tf_idf = await asyncio.gather(
+            self.get_formatted_entities(self.doc),
+            self.get_tfidf(self.doc)
+        )
+        return json.dumps({
+            "ents": fmt_ents,
+            "tf_idf": tf_idf
+            })
+
+    async def get_formatted_entities(self, doc: spacy.tokens.Doc):
+        return displacy.render(doc, style="ent")
+
+    async def get_tfidf(self, doc: spacy.tokens.Doc):
+        vectorizer = TfidfVectorizer(
+            ngram_range=(1, 1), max_features=20,
+            stop_words=self.nlp.Defaults.stop_words)
+        vectorizer.fit_transform([span.text for span in doc.sents])
+        tf_idf = dict(
+            feature for feature in zip(vectorizer.get_feature_names(), vectorizer.idf_)
+            if not feature[0].isnumeric()
+        )
+        return tf_idf
 
 
 if __name__ == "__main__":
-    print(get_formatted_entities(sys.argv[1]))
+    proc = processText(sys.argv[1])
+    print(asyncio.run(proc.process()))
