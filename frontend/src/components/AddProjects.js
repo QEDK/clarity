2cd3509597d92865771fdd9dbeb79ab2ab8089b6
Preet Shah <shahpreetk@gmail.com>
2020-11-09 19:59:43 +0530

    frontend: few name consistencies

--- frontend/src/components/AddProjects.js
+++ frontend/src/components/AddProjects.js
@@ -31,14 +31,14 @@ const AddProject = ({shouldShow = false}) => {
             {
                 show && (
                     <div className='add-project__input'>
-                        <input value={projectName} onChange={e=>setProjectName(e.target.value)} className='add-project__name' data-testid='project-name' type='text' placeholder='Name your Project ' />
+                        <input value={projectName} onChange={e=>setProjectName(e.target.value)} className='add-project__name' data-testid='project-name' type='text' placeholder='Name your List' />
                         <button className='add-project__submit' type='button' onClick={()=>addProject()} data-testid='add-project-submit'>Add List</button>
                         <span aria-label='Cancel adding project' data-testid='hide-project-overlay' className='add-project__cancel' onClick={()=>setShow(false)} onKeyDown={()=>setShow(false)} role='button' tabIndex={0}>Cancel</span>
                     </div>
                 )
             }
             <span className='add-project__plus'>+</span>
-            <span aria-label='Add Project' data-testid='add-project-action' className='add-project__text' onClick={()=>setShow(!show)} onKeyDown={()=>setShow(!show)} role='button' tabIndex={0}>Add List</span>
+            <span aria-label='Add List' data-testid='add-project-action' className='add-project__text' onClick={()=>setShow(!show)} onKeyDown={()=>setShow(!show)} role='button' tabIndex={0}>Add List</span>
         </div>
     )
 }
