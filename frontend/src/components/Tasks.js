882ee03b7151d5906f6b3b7ad6e6060b8063bd47
Preet Shah <shahpreetk@gmail.com>
2020-11-11 17:48:15 +0530

    frontend: code cleaning

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -28,35 +28,7 @@ class Tasks extends Component {
             })
             .catch(err => console.log(err))
     }
-    // const { selectedProject } = useSelectedProjectValue()
-    // const { projects } = useProjectsValue()
-    // const {tasks} = useTasks(selectedProject)
-    // const [tasks, setTasks] = useState('')
 
-    // let projectName = ''
-
-    // if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
-    //     projectName = getTitle(projects, selectedProject).name
-    // }
-
-    // if (collatedTasksExist(selectedProject) && selectedProject) {
-    //     projectName = getCollatedTitle(collatedTasks, selectedProject).name
-    // }
-
-    // useEffect((event) => {
-    //     document.title = `${projectName}: Clarity`
-    //     const base_url = process.env.REACT_APP_BASE_URL
-    //     const email = "shahpreetk@gmail.com"
-    //     axios.get(`${base_url}/get_note/${email}`)
-    //         .then(res => {
-    //             console.log(res.data)
-    //             setTasks(res.data)
-    //         })
-    //         .catch(err => console.log(err))
-
-    // if (JSON.stringify(allProjects) !== JSON.stringify(projects)){
-    //     setProjects(allProjects);
-    // });
     render() {
         const {tasks} = this.state
         return (
@@ -66,7 +38,7 @@ class Tasks extends Component {
                     {
                         tasks.map(task => (
                             <li key={`${task.id}`}>
-                                <Checkbox id={task.id} taskDesc={task.task} />
+                                {/* <Checkbox id={task.id} taskDesc={task.task} /> */}
                                 <span>{task.text_journal}</span>
                             </li>
                         ))
