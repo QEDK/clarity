eae34384c901a98e06f7d3c9e5e379953e312029
Preet Shah <shahpreetk@gmail.com>
2020-11-11 13:54:23 +0530

    frontend: add journal entry connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Content from './components/layout/Content';
+// import Login from './components/layout/Login';
 import {ProjectsProvider, SelectedProjectProvider} from './context'
 
 function App() {
+  const base_url = process.env.REACT_APP_BASE_URL
+  console.log(base_url)
   return (
+    <Router>
     <SelectedProjectProvider>
       <ProjectsProvider>
     <main data-testid='application'>
       <Header />
-      <Content />
+      <Switch>
+      <Route exact path='/' component={Content} />
+      {/* <Route exact path='/login' component={Login} /> */}
+      </Switch>
     </main>
     </ProjectsProvider>
     </SelectedProjectProvider>
+    </Router>
   );
 }
 
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import AddTask from '../AddTask';
+// import { AiOutlineUser , AiOutlineLogout} from 'react-icons/ai';
 
 const Header = () => {
     const [shouldShowMain, setShouldShowMain] = useState(false)
@@ -8,12 +10,15 @@ const Header = () => {
         <>
             <header className='header' data-testid='header'>
                 <nav>
+                    <Link to='/'>
                     <div className='logo'>
                         Clarity
                     </div>
+                    </Link>
                     <div className="settings">
                         <ul>
                             <li data-testid='quick-add-task-action' className='settings__add'><button type='button' aria-label='Quick Add Task' onClick={()=>{setShowQuickAddTask(true); setShouldShowMain(true)}} onKeyDown={()=>{setShowQuickAddTask(true); setShouldShowMain(true)}}>+</button></li>
+                            {/* <Link to='/login'><li data-testid='login' className='settings__add'><button type='button' aria-label='Login'><AiOutlineUser size={23} style={{'marginTop': '5px'}}/></button></li></Link> */}
                         </ul>
                     </div>
                 </nav>
